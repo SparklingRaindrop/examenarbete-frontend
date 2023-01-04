db0c8b24f525b66bf45351c6b397f43eaa31d56a
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 18:19:29 +0100

    Add design

--- src/components/List/styled.tsx
+++ src/components/List/styled.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
 export const UnorderedList = styled.ul`
-    
+    width: 100%;
+    padding: ${({ theme }) => theme.padding.md};
 `;
 
 export const StyledListItem = styled.li`
+    width: 100%;
+    padding: ${({ theme }) => `${theme.padding.sm} ${theme.padding.md}`};
     
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+    list-style: none;
 `;
