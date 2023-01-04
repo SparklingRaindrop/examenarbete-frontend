05521759d6447a51803970897369f1f7e65857c0
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 11:40:43 +0100

    Init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: true
+  },
   reactStrictMode: true,
+  swcMinify: true,
 }
-
-module.exports = nextConfig
