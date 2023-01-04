8fff91d4c6840b3af14c701e6285f6cf05ba6261
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 15:28:06 +0100

    Add new icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,8 +1,26 @@
+import { Path, path } from './path';
 import { Wrapper } from './styled'
 
-type Props = {}
-export default function Icon({ }: Props) {
-    return (
-        <Wrapper>Icon</Wrapper>
-    )
+type Props = {
+    name: Path;
 }
+
+export default function Icon(props: Props) {
+    const { name } = props;
+
+    return (
+        <Wrapper>
+            {
+                path[name].map((stroke, index) => (
+                    <path
+                        key={index}
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        d={stroke} />
+                ))
+            }
+        </Wrapper>
+    );
+}
+
+
--- src/components/Icon/styled.ts
+++ src/components/Icon/styled.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.svg`
+export const Wrapper = styled.svg.attrs(() => ({
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    strokeWidth: '1.5',
+    stroke: 'currentColor',
+}))`
     
 `;
