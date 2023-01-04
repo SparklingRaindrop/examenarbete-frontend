d79d8cda240255717f4df08bd5fe256d305b49ad
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 20:51:30 +0100

    Update design

--- src/components/Main/styled.ts
+++ src/components/Main/styled.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.main`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
     padding: 6rem;
     min-height: 100vh;
