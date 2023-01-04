7f2f8a44729de87b8a48ee5752656bec0e26e76c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 15:28:33 +0100

    Init

---
+++ src/pages/shoppingList.tsx
@@ -0,0 +1,10 @@
+import { List, ListItem } from '../components';
+
+type Props = {}
+export default function shoppingList({ }: Props) {
+    return (
+        <List>
+            <ListItem>Test</ListItem>
+        </List>
+    );
+}
