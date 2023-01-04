3ff476eeadea1388045e6cd9de4b15a9557c4975
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-04 17:52:47 +0100

    Add types

--- src/context/ThemeProvider/theme.ts
+++ src/context/ThemeProvider/theme.ts
@@ -1,38 +1,39 @@
 import { DefaultTheme } from 'styled-components';
+import { Border, Font, Palette } from '../../types/styled';
 
-const border = {
+const border: Border = {
     size: {
         thin: '1px',
         bold: '5px',
     }
 };
 
-const font = {
+const font: Font = {
     size: {
-        small: '1rem',
-        medium: '1.5rem',
+        small: '0.8rem',
+        standard: '1rem',
         large: '2rem',
     }
 };
 
-const palette = {
-    common: {
-        black: '#222831',
-        white: '#ffffff'
-    },
-    primary: {
-        main: '#726a95',
-        contrastText: '#ffffff'
-    },
-    secondary: {
-        main: '#709fb0',
-        contrastText: '#ffffff'
-    }
+export const palette = {
+    black: '#222831',
+    white: '#ffffff',
 };
 
-export const theme: DefaultTheme = {
+export const light = {
+    primary: 'tomato',
+    secondary: '#ffffff',
+    disabled: ''
+};
+
+export const dark = {
+    primary: '#709fb0',
+    secondary: '#ffffff',
+    disabled: ''
+};
+
+export const theme: Omit<DefaultTheme, 'palette' | 'toggleTheme'> = {
     border,
     font,
-    borderRadius: '4px',
-    palette,
 };
