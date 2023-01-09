5352d5c5e1913ce1f53b287ddb3b288eff6ba063
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 12:00:38 +0100

    Add disabled state

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -6,15 +6,17 @@ type Props = {
     onClick?: () => void;
     label?: string;
     variant?: Variant;
+    disabled?: boolean;
 } & GeneralProps;
 
 export default function Button(props: Props) {
-    const { onClick, label, children, variant } = props;
+    const { onClick, label, children, variant, disabled } = props;
 
     return (
         <Wrapper
             variant={variant}
-            onClick={onClick}>
+            onClick={onClick}
+            disabled={disabled}>
             {label}
             {children}
         </Wrapper>
