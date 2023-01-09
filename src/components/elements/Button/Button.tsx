d746d6a5269ccc688376b1a57777b7d97283a02a
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 12:09:39 +0100

    Change props

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from 'react';
 import { Variant } from '../../../types/styled';
 import { Wrapper } from './styled';
 
-type Props = {
+export interface ButtonProps extends GeneralProps {
     onClick?: () => void;
     label?: string;
     variant?: Variant;
     disabled?: boolean;
-} & GeneralProps;
+};
 
-export default function Button(props: Props) {
+export default function Button(props: ButtonProps) {
     const { onClick, label, children, variant, disabled } = props;
 
     return (
--- src/components/elements/Button/IconButton.tsx
+++ src/components/elements/Button/IconButton.tsx
@@ -1,16 +1,17 @@
-import { cloneElement, ReactElement } from 'react';
-import Button from './Button';
+import { Icon } from '../Icon';
+import { Path } from '../Icon/path';
+import Button, { ButtonProps } from './Button';
 
 type Props = {
-    icon: ReactElement;
-}
+    name: string;
+} & ButtonProps;
 
 export default function IconButton(props: Props) {
-    const { icon } = props;
+    const { name, disabled, onClick } = props;
 
     return (
-        <Button>
-            {cloneElement(icon)}
+        <Button onClick={onClick} disabled={disabled}>
+            <Icon name={name as Path} />
         </Button>
     );
 }
