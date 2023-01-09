20429ea8515a072d703673b5e909e2e54d89c14c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 11:58:46 +0100

    Add styles

--- src/components/elements/Counter/styled.ts
+++ src/components/elements/Counter/styled.ts
@@ -3,5 +3,11 @@ import styled from 'styled-components';
 export const Input = styled.input.attrs(() => ({
     type: 'number'
 }))`
-    
+    width: ${({ theme }) => `calc(${theme.font.size.standard}*1.5)`};
+    text-align: center; 
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
 `;
