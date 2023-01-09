9dc7ecdf190eabcfb97bc337776bea3cd9574a9c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 15:20:05 +0100

    Add min value on input

--- src/components/elements/Counter/styled.ts
+++ src/components/elements/Counter/styled.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Input = styled.input.attrs(() => ({
-    type: 'number'
+    type: 'number',
+    min: '0'
 }))`
     width: ${({ theme }) => `calc(${theme.font.size.standard}*1.5)`};
     text-align: center; 
