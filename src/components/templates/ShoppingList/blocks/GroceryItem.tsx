53880879c6a8e2f290c835c3db2558a22aa82744
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-09 20:45:50 +0100

    Add functions to make "edit item" possible

--- src/components/templates/ShoppingList/blocks/GroceryItem.tsx
+++ src/components/templates/ShoppingList/blocks/GroceryItem.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, useMemo } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { useGroceries } from '../../../../hooks';
-import { Checkbox, Counter, IconButton, ListItem } from '../../../elements';
+import { Checkbox, Counter, IconButton, Input } from '../../../elements';
+import { Wrapper } from './styled';
 
 interface Props extends Grocery {
     toggleCheckbox: (event: ChangeEvent<HTMLInputElement>, id: Pick<Grocery, 'id'>) => void;
@@ -8,11 +9,18 @@ interface Props extends Grocery {
 
 export default function GroceryItem(props: Props) {
     const { id, toggleCheckbox, } = props;
-    const { editItem, groceries } = useGroceries();
+    const [isEditing, setIsEditing] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const { editItem, removeItem, groceries } = useGroceries();
     const grocery = useMemo<Grocery | undefined>(() => groceries.find(item => item.id === id), [groceries, id]);
+    const item_name = useMemo<string>(() => grocery?.item_name || ' ', [grocery]);
+
+    useEffect(() => {
+        setInputValue(item_name);
+    }, [item_name]);
 
     if (!grocery) return <></>;
-    const { amount, item_name, isChecked } = grocery;
+    const { amount, isChecked } = grocery;
 
     function updateCounterValue(value: number, action?: string): void {
         if (value < 0 && amount === 0) return;
@@ -21,18 +29,38 @@ export default function GroceryItem(props: Props) {
         });
     }
 
+    function updateItemName(value: string) {
+        setInputValue(value);
+    }
+
     return (
-        <ListItem>
+        <Wrapper>
             <Checkbox
-                label={item_name || ' '}
                 checked={isChecked}
                 toggle={(event) => toggleCheckbox(event, id as unknown as Pick<Grocery, 'id'>)}
                 crossOffOnChecked />
+            {
+                isEditing ?
+                    <Input
+                        value={inputValue}
+                        variant='ghost'
+                        onChange={updateItemName}
+                        onBlur={async () => {
+                            setIsEditing(false);
+                            editItem(id as unknown as Pick<Grocery, 'id'>, {
+                                item_name: inputValue,
+                            });
+                        }}
+                        autoFocus /> :
+                    <label onClick={() => setIsEditing(true)}>{item_name || ' '}</label>
+            }
             <Counter
                 value={amount}
                 setCounterValue={updateCounterValue}
             />
-            <IconButton name='delete' />
-        </ListItem>
+            <IconButton
+                name='delete'
+                onClick={() => removeItem(id as unknown as Pick<Grocery, 'id'>)} />
+        </Wrapper>
     );
 }
