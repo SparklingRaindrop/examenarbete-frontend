f929248b08356a7b3dd915a16ff7523cd6abdb08
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-10 17:39:36 +0100

    Remove AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '../context';
-import { AuthProvider } from '../context/AuthProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
