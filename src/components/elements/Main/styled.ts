85eb18a4f6e78075e4a5ffb92ef0c589c31625ae
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-16 21:03:11 +0100

    Add margin-top

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.main`
+    min-height: 100vh;
+    margin-top: 3.5rem; // Header height
+    padding: ${({ theme }) => theme.padding.md};
+
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: ${({ theme }) => theme.padding.md};
-    min-height: 100vh;
 `;
