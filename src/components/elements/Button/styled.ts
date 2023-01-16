8d5f3da07b3a2951aa6164e911e0318935727dc8
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-16 21:47:15 +0100

    Add colors to button

--- src/components/elements/Button/styled.ts
+++ src/components/elements/Button/styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { theme } from '../../../context/ThemeProvider/theme';
 import { Variant } from '../../../types/styled';
 
 interface ButtonProps {
@@ -6,8 +7,13 @@ interface ButtonProps {
 }
 
 export const Wrapper = styled.button<ButtonProps>`
-    min-width: 1.5rem;
-    min-height: 1.5rem;
+    padding: 1rem 1.5rem;
+
+    font-size: 1rem;
+    color: ${({ theme, variant }) => variant === 'secondary' ? theme.palette.black : theme.palette.white};
+    text-transform: capitalize;
+
+    border-radius: 2em;
 
     display: flex;
     align-items: center;
@@ -18,25 +24,58 @@ export const Wrapper = styled.button<ButtonProps>`
             case 'ghost':
                 return 'transparent';
                 break;
-
+            case 'secondary':
+                return theme.palette.white;
+                break;
             default:
-                return theme.palette.primary;
+                return theme.palette.primary.original;
                 break;
         }
     }};
-    border-color: ${({ variant, theme }) => {
+
+    border: ${({ variant, theme }) => variant === 'secondary' ? theme.palette.secondary.original : 'transparent'} 2px solid;
+
+    &:hover {
+        cursor: pointer;
+            background-color: ${({ variant, theme }) => {
         switch (variant) {
             case 'ghost':
                 return 'transparent';
                 break;
-
+            case 'secondary':
+                return `${theme.palette.secondary.light}20`;
+                break;
             default:
-                return theme.palette.primary;
+                return theme.palette.primary.shade;
                 break;
         }
     }};
-
-    &:hover {
-        cursor: pointer;
     }
+
+    &:active {
+        background-color: ${({ variant, theme }) => {
+        switch (variant) {
+            case 'ghost':
+                return 'transparent';
+                break;
+            case 'secondary':
+                return theme.palette.secondary.light;
+                break;
+            default:
+                return theme.palette.primary.light;
+                break;
+        }
+    }};
+    }
+
+    &:disabled {
+        background-color: rgba(79, 66, 120, 0.44);
+        color:  rgba(244, 247, 240, 0.59);
+    }
+
+`;
+
+export const IconButtonWrapper = styled(Wrapper)`
+    min-width: 1.5rem;
+    min-height: 1.5rem;
 `;
