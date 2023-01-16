298c9e34a7b8fc52bd444265308e2d1db0f6a684
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-16 21:02:56 +0100

    Add font*

--- src/components/elements/Main/Main.tsx
+++ src/components/elements/Main/Main.tsx
@@ -1,12 +1,15 @@
-import { ReactNode } from 'react';
 import { Wrapper } from './styled';
+import { Heebo } from '@next/font/google';
+const heebo = Heebo({
+    variable: '--text-font',
+});
 
 type Props = GeneralProps;
 
 export default function Main(props: Props) {
     const { children } = props;
     return (
-        <Wrapper>
+        <Wrapper className={heebo.variable}>
             {children}
         </Wrapper>
     )
