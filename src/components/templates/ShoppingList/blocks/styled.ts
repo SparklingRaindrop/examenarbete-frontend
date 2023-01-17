4299e44503dbbdb89cbdc95ad0d85483fcfab927
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-17 14:54:39 +0100

    Change padding

--- src/components/templates/ShoppingList/blocks/styled.ts
+++ src/components/templates/ShoppingList/blocks/styled.ts
@@ -3,7 +3,7 @@ import { StyledListItem } from '../../../elements/List/styled';
 
 export const Wrapper = styled(StyledListItem)`
     max-width: 100%;
-    padding: ${({ theme }) => theme.padding.sm};
+    padding: ${({ theme }) => `${theme.padding.sm} 0`};
 
     gap: 1rem;
     justify-content: center;
