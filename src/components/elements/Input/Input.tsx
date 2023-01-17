8533bf70a3266697644d87400517c0676ccfdacf
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-17 10:55:20 +0100

    Added styles on Input

--- src/components/elements/Input/Input.tsx
+++ src/components/elements/Input/Input.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, CSSProperties, HTMLInputTypeAttribute } from 'react';
 import { Variant } from '../../../types/styled';
-import { Wrapper } from './styled';
+import { ErrorMessage, Wrapper } from './styled';
 
 type Props = {
     value: number | string;
@@ -8,11 +8,35 @@ type Props = {
     autoFocus?: boolean;
     type?: HTMLInputTypeAttribute;
     id?: string;
+    isError: boolean;
+    error?: string;
+    required?: boolean;
     onChange?: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-    onBlur?: () => void;
 }
 
 export default function Input(props: Props & CSSProperties) {
+    const { value, autoFocus, id, type, isError, required, error, onChange } = props;
+
+    return (
+        <div>
+            <Wrapper
+                id={id}
+                value={value}
+                type={type}
+                autoFocus={autoFocus}
+                isError={isError}
+                required={required}
+                onChange={onChange} />
+            {isError &&
+                <ErrorMessage>
+                    {error}
+                </ErrorMessage>
+            }
+        </div>
+    );
+}
+
+/* export function SPInput(props: Props & CSSProperties) {
     const { value, variant, autoFocus, id, type, onChange, onBlur } = props;
 
     return (
@@ -25,4 +49,4 @@ export default function Input(props: Props & CSSProperties) {
             type={type}
             autoFocus={autoFocus} />
     );
-}
+} */
--- src/components/elements/Input/styled.ts
+++ src/components/elements/Input/styled.ts
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 import { Variant } from '../../../types/styled';
 
-type InputVariant = { variant?: Variant };
-
 export const Wrapper = styled.input.attrs(({ type }) => ({
     type: type || 'text',
-})) <InputVariant>`
-    background: ${({ variant }) => variant === 'ghost' && 'transparent'};
-    border: ${({ variant }) => variant === 'ghost' && 'none'};
+})) <{ isError: boolean }>`
+    padding: ${({ theme }) => theme.padding.md};
+    
+    outline: none;
+    border-radius: ${({ theme }) => theme.borderRadius.md};
+    background: ${({ theme }) => theme.palette.white}80;
+    border: ${({ theme, isError }) =>
+        `${theme.border.bold} ${isError ? theme.palette.accent.original : theme.palette.black + '80'}`} solid;
+
+    &:focus {
+        border: ${({ theme }) =>
+        `${theme.border.bold} ${theme.palette.primary.original}`} solid;
+    }
+
+    &:focus {
+        border: ${({ theme }) =>
+        `${theme.border.bold} ${theme.palette.primary.original}`} solid;
+    }
+
+    &:invalid {
+        border: ${({ theme }) =>
+        `${theme.border.bold} ${theme.palette.accent.original}`} solid;
+    }
 `;
 
 export const InputGroup = styled.div`
@@ -32,4 +50,10 @@ export const InputGroup = styled.div`
 
 export const InputRightElement = styled.div`
     padding: 2px;
+`;
+
+export const ErrorMessage = styled.div`
+    color: ${({ theme }) => theme.palette.accent.original};
+    font-size: ${({ theme }) => theme.font.size.sm};
+    padding: 0.5em 0 ${({ theme }) => theme.padding.sm};
 `;
