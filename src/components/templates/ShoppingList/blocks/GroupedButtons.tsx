b13bf854d55aec7d772fdcefaa6899497b628836
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-17 14:19:57 +0100

    First commit

---
+++ src/components/templates/ShoppingList/blocks/GroupedButtons.tsx
@@ -0,0 +1,12 @@
+import { Button, IconButton } from '../../../elements';
+import { Flex } from './styled';
+
+type Props = {}
+export default function GroupedButtons({ }: Props) {
+    return (
+        <Flex>
+            <Button label='Generate from meal plan' />
+            <IconButton name='threeDots' variant='ghost' />
+        </Flex>
+    )
+}
