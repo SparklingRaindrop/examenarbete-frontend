eab7f7a8ae82f0b7515c6f3b453a75407402ad18
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-18 16:41:46 +0100

    Add context wrapper

--- src/pages/user/plans/index.tsx
+++ src/pages/user/plans/index.tsx
@@ -1,11 +1,14 @@
 import { MealPlans } from '../../../components/templates';
 import { MealPlansProvider } from '../../../context';
+import { RecipesProvider } from '../../../context/RecipesProvider';
 
 type Props = {}
 export default function MealPlanPage({ }: Props) {
     return (
         <MealPlansProvider>
-            <MealPlans />
+            <RecipesProvider>
+                <MealPlans />
+            </RecipesProvider>
         </MealPlansProvider>
     );
 }
