7dd225e877d211429c2bfeecfd6c51539b7ef76c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-18 11:41:05 +0100

    Rename Plans to DailyPlan & Separate Plans.

--- src/components/templates/MealPlans/MealPlans.tsx
+++ src/components/templates/MealPlans/MealPlans.tsx
@@ -4,8 +4,82 @@ import { IconButton } from '../..';
 import useMealPlansContext from '../../../hooks/useMealPlansContext';
 
 import Calendar from './blocks/Calendar';
+import Plans from './blocks/Plans';
 
 type Props = {}
+
+const sample: Plan[] = [
+    {
+        id: 'plan8',
+        date: new Date(2023, 0, 18),
+        updated_at: new Date(),
+        type: 'breakfast',
+        recipe: {
+            title: 'test18',
+            id: 'recipe11',
+        }
+    },
+    {
+        id: 'plan9',
+        date: new Date(2023, 0, 16),
+        updated_at: new Date(),
+        type: 'lunch',
+        recipe: {
+            title: 'test16',
+            id: 'recipe12',
+        }
+    },
+    {
+        id: 'plan10',
+        date: new Date(2023, 0, 17),
+        updated_at: new Date(),
+        type: 'dinner',
+        recipe: {
+            title: 'test17',
+            id: 'recipe13',
+        }
+    },
+    {
+        id: 'plan11',
+        date: new Date(2023, 0, 18),
+        updated_at: new Date(),
+        type: 'lunch',
+        recipe: {
+            title: 'test18',
+            id: 'recipe14',
+        }
+    },
+    {
+        id: 'plan12',
+        date: new Date(2023, 0, 19),
+        updated_at: new Date(),
+        type: 'dinner',
+        recipe: {
+            title: 'test19',
+            id: 'recipe15',
+        }
+    },
+    {
+        id: 'plan13',
+        date: new Date(2023, 0, 20),
+        updated_at: new Date(),
+        type: 'dinner',
+        recipe: {
+            title: 'test20',
+            id: 'recipe15',
+        }
+    },
+    {
+        id: 'plan14',
+        date: new Date(2023, 0, 21),
+        updated_at: new Date(),
+        type: 'dinner',
+        recipe: {
+            title: 'test21',
+            id: 'recipe15',
+        }
+    }
+];
 
 export default function MealPlans({ }: Props) {
     const [selectedDates, setSelectedDates] = useState<Date[]>([new Date()]);
@@ -30,20 +104,45 @@ export default function MealPlans({ }: Props) {
 
     }
 
+    function getTargetPlans(): Plan[][] {
+        // Filtered by selected date
+        const filtered = sample.filter(({ date }) => {
+            const targetDate = new Date(date).getDate();
+            if (selectedDates.length === 1) {
+                return selectedDates[0].getDate() === targetDate;
+            } else {
+                return selectedDates[0].getDate() <= targetDate &&
+                    targetDate <= selectedDates[1].getDate();
+            }
+        });
+        // Divide into each date
+        const divided = filtered.reduce((result: Plan[][], currentPlan: Plan, index) => {
+            const currentDate = currentPlan.date.getDate();
+            if (index === 0 || result[result.length - 1][0].date.getDate() !== currentDate) {
+                result.push([currentPlan]);
+                return result;
+            }
+            result[result.length - 1].push(currentPlan);
+            return result;
+        }, []);
+        // Sort in ascending order
+        return divided.sort((a, b) => {
+            const aDate = a[0].date.getDate();
+            const bDate = b[0].date.getDate();
+            if (aDate < bDate) {
+                return -1;
+            }
+            if (aDate > bDate) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
     return (
         <>
             <Calendar selectedDates={selectedDates} addSelectedDate={addSelectedDate} />
-            <div>
-                <h3>Breakfast</h3>
-                <IconButton name='plus' />
-                {plans.filter(plan => plan.type === 'breakfast').map(plan => <div key={plan.id}>{plan.recipe.title}</div>)}
-                <h3>Lunch</h3>
-                <IconButton name='plus' />
-                {plans.filter(plan => plan.type === 'lunch').map(plan => <div key={plan.id}>{plan.recipe.title}</div>)}
-                <h3>Dinner</h3>
-                <IconButton name='plus' />
-                {plans.filter(plan => plan.type === 'dinner').map(plan => <div key={plan.id}>{plan.recipe?.title}</div>)}
-            </div>
+            <Plans filteredPlans={getTargetPlans()} />
         </>
     );
 }
