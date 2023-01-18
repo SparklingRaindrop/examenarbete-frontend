b8b3e17e76c97ecbaaf7bd32cdd8fb780acf12c1
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-18 16:42:56 +0100

    Move from DailyPlan

--- src/components/templates/MealPlans/blocks/Plans/styled.ts
+++ src/components/templates/MealPlans/blocks/Plans/styled.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const H3 = styled.h3`
-width: 100%;
-border-bottom: ${({ theme }) => theme.palette.accent.original} 1px solid;
+    width: 100%;
+    border-bottom: ${({ theme }) => theme.palette.accent.original} 1px solid;
 `;
 
 export const Wrapper = styled.div`
@@ -12,8 +12,24 @@ export const Wrapper = styled.div`
     align-items: center;
     flex-direction: column;
     gap: 1rem;
+`;
 
-    &:not(:first-of-type) {
-        margin-top: 2rem;
-    }
+export const Meal = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+    gap: 1rem;
+`;
+
+export const MealName = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.5rem;
+    justify-content: space-between;
+    gap: 0.25rem;
+    text-transform: capitalize;
 `;
