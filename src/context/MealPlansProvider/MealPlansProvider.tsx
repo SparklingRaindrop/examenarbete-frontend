dbebeeee58f3ba69311f75c188e10450c8af1366
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-18 13:27:46 +0100

    Add remove function

--- src/context/MealPlansProvider/MealPlansProvider.tsx
+++ src/context/MealPlansProvider/MealPlansProvider.tsx
@@ -3,16 +3,18 @@ import useMealPlansAPI from '../../hooks/useMealPlansAPI';
 
 export interface ContextMealPlans {
     plans: Plan[];
+    removePlan: (id: Plan['id']) => void;
 }
 
 export const MealPlansContext = createContext<ContextMealPlans | null>(null);
 
 export function MealPlansProvider(props: GeneralProps) {
     const { children } = props;
-    const { plans } = useMealPlansAPI();
+    const { plans, removePlan } = useMealPlansAPI();
 
     const value = {
         plans,
+        removePlan,
     };
 
     return (
--- src/hooks/useMealPlansAPI.tsx
+++ src/hooks/useMealPlansAPI.tsx
@@ -1,23 +1,37 @@
-import nodeTest from 'node:test';
 import { useEffect, useState } from 'react';
 import { Status } from '../types/statusCode';
-import { get, isGetResponse } from '../util/api';
+import { APIResponse, get, isGetResponse, remove } from '../util/api';
 
 export default function useMealPlansAPI() {
     const [plans, setPlans] = useState<Plan[]>([]);
 
     useEffect(() => {
         async function init() {
-            const response = await get<Plan[]>('/plans');
-            if (response && response.status === Status.Succuss && isGetResponse(response)) {
-                const { data } = response;
-                setPlans(data);
-            }
+            getPlans();
         }
         init();
     }, []);
 
+    // TODO: fetch only current month
+    async function getPlans(): Promise<APIResponse> {
+        const response = await get<Plan[]>('/plans');
+        if (response && response.status === Status.Succuss && isGetResponse(response)) {
+            const { data } = response;
+            setPlans(data);
+        }
+        return { status: response.status };
+    }
+
+    async function removePlan(id: Plan['id']): Promise<APIResponse> {
+        const response = await remove(`/plans/${id}`);
+        if (response && response.status === Status.NoContent) {
+            getPlans();
+        }
+        return { status: response.status };
+    }
+
     return {
-        plans
+        plans,
+        removePlan
     };
 }
