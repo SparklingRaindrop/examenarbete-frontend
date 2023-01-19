3f4052ff5618c443504e54d2b29607dfd4ff3d6a
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-19 16:46:59 +0100

    Implement RecipeList

--- src/components/templates/Recipes/Recipes.tsx
+++ src/components/templates/Recipes/Recipes.tsx
@@ -1,6 +1,40 @@
-type Props = {}
-export default function Recipes({ }: Props) {
+import { useEffect, useState } from 'react';
+
+import { useRecipesContext } from '../../../hooks';
+
+import { IconButton } from '../../elements';
+
+import { RecipeEditor } from './blocks';
+import { RecipeList } from './blocks';
+
+export default function Recipes() {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const { getRecipes } = useRecipesContext();
+
+    useEffect(() => {
+        updateRecipes();
+        // eslint-disable-next-line
+    }, []);
+
+    async function updateRecipes(): Promise<void> {
+        const response = await getRecipes();
+        if (response.data) {
+            setRecipes(response.data);
+        }
+    }
+
     return (
-        <div>Recipes</div>
+        <>
+            <h3>
+                Search
+            </h3>
+            <input />
+            <h3>
+                My Recipes
+            </h3>
+            <IconButton name='plus' />
+            <RecipeList recipes={recipes} updateRecipes={updateRecipes} />
+            {<RecipeEditor />}
+        </>
     );
 }
