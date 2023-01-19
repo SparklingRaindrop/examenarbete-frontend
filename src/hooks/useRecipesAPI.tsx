1564ba238167e22272ba21c687fb934c68d595d7
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-19 15:12:56 +0100

    Change keyword to optional

--- src/hooks/useRecipesAPI.tsx
+++ src/hooks/useRecipesAPI.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
 import { Status } from '../types/statusCode';
 import { APIResponse, get, GetResponse, isGetResponse, remove } from '../util/api';
 
 export default function useRecipesAPI() {
 
-    async function getRecipes(keyword: string): Promise<GetResponse<Recipe[]> | APIResponse> {
+    async function getRecipes(keyword?: string): Promise<GetResponse<Recipe[]> | APIResponse> {
         const response = await get<Recipe[]>(`/recipes${keyword && '?keyword=' + keyword}`);
         if (response && response.status === Status.Succuss && isGetResponse(response)) {
             const { data } = response;
