6525a68f41718e9e44601bdc26906ab0d2f44e20
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-20 18:45:47 +0100

    Add items and filterItems

--- src/context/RecipesProvider/RecipesProvider.tsx
+++ src/context/RecipesProvider/RecipesProvider.tsx
@@ -4,27 +4,40 @@ import { RecipeRequestData } from '../../hooks/useRecipesAPI';
 import { APIResponse, GetResponse } from '../../util/api';
 
 export interface ContextRecipes {
+    items: Item[];
+    filterItems: (keyword?: string) => Item[];
     getRecipes: (keyword?: string) => Promise<GetResponse<Recipe[]>>;
     getItems: (keyword?: string) => Promise<GetResponse<Item[]>>;
     getRecipe: (id: string) => Promise<GetResponse<Recipe>>;
     removeRecipe: (id: Recipe['id']) => Promise<APIResponse>;
     getUnits: () => Promise<GetResponse<Unit[]>>;
-    addPlan: (newData: RecipeRequestData) => Promise<APIResponse>;
+    addRecipe: (newData: RecipeRequestData) => Promise<APIResponse>;
 }
 
 export const RecipesContext = createContext<ContextRecipes | null>(null);
 
 export function RecipesProvider(props: GeneralProps) {
     const { children } = props;
-    const { getRecipes, removeRecipe, getRecipe, getItems, getUnits, addPlan } = useRecipesAPI();
+    const {
+        items,
+        filterItems,
+        getRecipes,
+        removeRecipe,
+        getRecipe,
+        getItems,
+        getUnits,
+        addRecipe
+    } = useRecipesAPI();
 
     const value = {
+        items,
+        filterItems,
         getRecipes,
         getRecipe,
         removeRecipe,
         getItems,
         getUnits,
-        addPlan
+        addRecipe
     };
 
     return (
--- src/hooks/useRecipesAPI.tsx
+++ src/hooks/useRecipesAPI.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Status } from '../types/statusCode';
-import { APIResponse, get, GetResponse, isGetResponse, remove } from '../util/api';
+import { APIResponse, get, GetResponse, post, remove } from '../util/api';
+
+export interface RecipeData extends Pick<Recipe, 'title'> {
+    ingredients: {
+        item: Item,
+        amount: number;
+    }[];
+    instructions: Array<Omit<Instruction, 'id'> & { id?: Instruction['id'] }>;
+}
+
+export interface RecipeRequestData extends Pick<Recipe, 'title'> {
+    ingredients: Array<{
+        amount: number;
+        item_id: Item['id'];
+    }>,
+    instructions: Array<Omit<Instruction, 'id'> & { id?: Instruction['id'] }>;
+}
 
 export default function useRecipesAPI() {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        async function init() {
+            const response = await getItems();
+            if (response.status === Status.Succuss && response.data) {
+                setItems(response.data);
+            }
+        }
+        init();
+    }, []);
 
     async function getRecipes(keyword?: string): Promise<GetResponse<Recipe[]>> {
         const response = await get<Recipe[]>(`/recipes${keyword ? '?keyword=' + keyword : ''}`);
@@ -31,11 +59,26 @@ export default function useRecipesAPI() {
         return response;
     }
 
+    async function addRecipe(newData: RecipeRequestData): Promise<APIResponse> {
+        const response = await post<RecipeRequestData>('/recipes', newData);
+        return response;
+    }
+
+    function filterItems(keyword?: string) {
+        if (!keyword) return items;
+
+        const key = keyword.toLowerCase();
+        return items.filter(item => item.name.includes(key));
+    }
+
     return {
+        items,
         getRecipes,
         getRecipe,
         removeRecipe,
         getItems,
         getUnits,
+        addRecipe,
+        filterItems,
     };
 }
