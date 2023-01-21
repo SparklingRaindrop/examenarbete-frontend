16c81edd621f621307f8d371ecd4a54b9fa043db
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-21 19:11:31 +0100

    Add "add item" function

--- src/components/templates/StockManager/StockManager.tsx
+++ src/components/templates/StockManager/StockManager.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
+import { useRecipesContext } from '../../../hooks';
 import useStocksContext from '../../../hooks/useStocksContext';
+import { InputTogglingButton, ItemInputFields } from '../../elements';
 import { StockItem } from './blocks';
 
 type Props = {}
 export default function StockManager({ }: Props) {
-    const { stocks } = useStocksContext();
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const { stocks, addNewItemToStocks } = useStocksContext();
+
     return (
         <div>
             <ul>
                 {
-                    stocks.map((stock) => (
+                    stocks.map(({ id }) => (
                         <StockItem
-                            key={stock.id}
-                            stock={stock} />
+                            key={id}
+                            id={id} />
                     ))
                 }
             </ul>
+            <InputTogglingButton
+                isEditing={isEditing}
+                inputElement={
+                    <ItemInputFields
+                        addItem={(item) => addNewItemToStocks(item)}
+                        onClose={() => setIsEditing(false)} />
+                }
+                onOpen={() => setIsEditing(true)}
+            />
         </div>
     );
 }
