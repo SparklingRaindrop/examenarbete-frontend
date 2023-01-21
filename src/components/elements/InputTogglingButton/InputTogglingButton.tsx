93725c759b21bb0cb1a61ede11066a563dc513c3
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-21 17:59:34 +0100

    Add basic functionality

--- src/components/elements/InputTogglingButton/InputTogglingButton.tsx
+++ src/components/elements/InputTogglingButton/InputTogglingButton.tsx
@@ -1,6 +1,24 @@
-type Props = {}
-export default function InputTogglingButton({ }: Props) {
-    return (
-        <div>InputTogglingButton</div>
-    )
+import { cloneElement, ReactElement } from 'react';
+import { Button } from '../Button';
+
+type Props = {
+    isEditing: boolean;
+    onOpen: () => void;
+    label?: string;
+    inputElement: ReactElement;
+}
+
+export default function InputTogglingButton(props: Props) {
+    const { isEditing, label, inputElement, onOpen } = props;
+
+    if (!isEditing) {
+        return (
+            <Button
+                onClick={onOpen}
+                label={label || 'add an item'} />
+        );
+    } else {
+        return cloneElement(inputElement);
+    }
+
 }
