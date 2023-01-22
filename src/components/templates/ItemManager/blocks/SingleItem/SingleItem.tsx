558c5cd3fb29eacee822e76f39e68d2418bce2b5
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-22 14:44:44 +0100

    Implement ItemEditor

--- src/components/templates/ItemManager/blocks/SingleItem/SingleItem.tsx
+++ src/components/templates/ItemManager/blocks/SingleItem/SingleItem.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import { useRecipesContext } from '../../../../../hooks';
 import { IconButton } from '../../../../elements';
+import ItemEditor from '../ItemEditor';
 
 export default function SingleItem(props: Item) {
     const { unit, name, id } = props;
-    const { items, removeItem } = useRecipesContext();
+    const { removeItem } = useRecipesContext();
+    const [isEditing, setIsEditing] = useState<boolean>();
 
     return (
         <li key={id}>
-            {name} {unit.name}
-            <IconButton
-                name='pencil'
-                variant='ghost' />
-            <IconButton
-                name='xMark'
-                variant='ghost'
-                onClick={() => removeItem(id)} />
+            {isEditing ?
+                <ItemEditor
+                    {...props}
+                    onClose={() => setIsEditing(false)} /> : (
+                    <>
+                        {name} {unit.name}
+                        <IconButton
+                            name='pencil'
+                            variant='ghost'
+                            onClick={() => setIsEditing(true)} />
+                        <IconButton
+                            name='xMark'
+                            variant='ghost'
+                            onClick={() => removeItem(id)} />
+                    </>
+                )}
         </li>
-    )
+    );
 }
