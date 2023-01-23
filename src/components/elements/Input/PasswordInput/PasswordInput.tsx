cc1cebe50248c36cd8b55201f96564dc8d95e55b
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-23 21:36:03 +0100

    Add handleKeydown

--- src/components/elements/Input/PasswordInput/PasswordInput.tsx
+++ src/components/elements/Input/PasswordInput/PasswordInput.tsx
@@ -1,22 +1,33 @@
-import { ChangeEvent, useState } from 'react';
-import { IconButton } from '../../Button'
-import Input from '../Input'
-import { InputGroup, InputRightElement } from '../styled'
+import { eventNames } from 'process';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { IconButton } from '../../Button';
+import Input from '../Input';
+import { InputGroup, InputRightElement } from '../styled';
 
 type Props = {
     value: string;
     onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    handleSubmit: () => void;
 }
 
 export default function PasswordInput(props: Props) {
-    const { value, onChange } = props;
+    const { value, onChange, handleSubmit } = props;
     const [isShown, setIsShown] = useState<boolean>(false);
+
+    function handleOnKeyDown(event: KeyboardEvent<Element>): void {
+        if (event.key === 'Enter') {
+            handleSubmit();
+        }
+    }
+
     return (
         <InputGroup>
             <Input
                 id='password'
+                placeholder='password'
                 value={value}
                 type={isShown ? 'text' : 'password'}
+                onKeyDown={handleOnKeyDown}
                 onChange={onChange} />
             <InputRightElement>
                 <IconButton
