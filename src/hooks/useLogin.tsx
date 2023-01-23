1f12d2dd9715d2c2c9d8cc6d7eaf3ee9844f923b
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-23 09:31:32 +0100

    Update func: separate from functions on api.ts

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,30 +1,41 @@
+import axios from 'axios';
 import Cookie from 'js-cookie';
 import { Status } from '../types/statusCode';
-import { APIResponse, isPostResponse, post } from '../util/api';
-
-type LoginOption = 'username' | 'email';
+import { APIResponse } from '../util/api';
 
 export interface Token {
-    token: string | null;
+    accessToken: string | null;
+    sessionToken: string | null;
+    expires: Date;
 }
 
-export type LoginData = {
-    password: string;
-} & {
-        [key in LoginOption as string]: string
-    };
+export type LoginData = { password: string; } & Partial<Pick<User, 'username' | 'email'>>
 
 export function useLogin(): {
     login: (data: LoginData) => Promise<APIResponse>;
 } {
     async function login(data: LoginData): Promise<APIResponse> {
-        const response = await post<Token>('/auth/login', data);
-        const { status, error } = response;
-        if (isPostResponse(response) && status === Status.Created) {
-            const { token, expires } = response.data;
-            Cookie.set('token', JSON.stringify(token), expires);
-        }
-        return { status, error };
+        try {
+            const response = await axios.post<Token>(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/login`, data, {
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
+
+            if (response && response.data && response.status === Status.Created) {
+                const { accessToken, sessionToken, expires } = response.data;
+                Cookie.set('access_token', JSON.stringify(accessToken), { expires });
+                Cookie.set('session_token', JSON.stringify(sessionToken));
+            }
+            return {
+                status: response.status
+            };
+        } catch (error: unknown) {
+            console.error(error);
+        };
+        return {
+            status: Status.BadRequest,
+        };
     }
 
     return { login };
