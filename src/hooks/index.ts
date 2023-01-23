ba2fda96de2d8874f61409bb2f8780fd902d763c
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-23 20:58:19 +0100

    First commit & implement: UserContext

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,6 +8,7 @@ import useTheme from './useTheme';
 import { useLogin } from './useLogin';
 import useDisclosure from './useDisclosure';
 import { useCalendar } from './useCalendar';
+import useUserContext from './useUserContext';
 
 export {
     useGroceriesContext,
@@ -19,5 +20,6 @@ export {
     useMealPlansContext,
     useMealPlansAPI,
     useRecipesAPI,
-    useRecipesContext
+    useRecipesContext,
+    useUserContext
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,22 +5,25 @@ import { GroceriesProvider, MealPlansProvider, RecipesProvider, ThemeProvider } 
 
 import { Layout } from '../components';
 import { StocksProvider } from '../context/StocksProvider';
+import { UserProvider } from '../context/UserContext/UserContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider>
-        <StocksProvider>
-          <RecipesProvider>
-            <GroceriesProvider>
-              <MealPlansProvider>
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </MealPlansProvider>
-            </GroceriesProvider>
-          </RecipesProvider>
-        </StocksProvider>
+        <UserProvider>
+          <StocksProvider>
+            <RecipesProvider>
+              <GroceriesProvider>
+                <MealPlansProvider>
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                </MealPlansProvider>
+              </GroceriesProvider>
+            </RecipesProvider>
+          </StocksProvider>
+        </UserProvider>
       </ThemeProvider></>
   );
 }
