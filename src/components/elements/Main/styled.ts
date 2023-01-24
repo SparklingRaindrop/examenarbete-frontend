6a542321dfaa8a7fabe7ad8203f69654d5fba713
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 13:57:52 +0100

    Fix scrollbar issue

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -5,9 +5,6 @@ export const Wrapper = styled.main`
     max-width: 100vw;
     margin-top: 3.5rem; // Header height
     padding: ${({ theme }) => theme.padding.md};
-    
-    overflow-x: hidden;
-    overflow-y: auto;
 
     display: flex;
     flex-direction: column;
--- src/components/layout/Footer/styled.ts
+++ src/components/layout/Footer/styled.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.footer`
-    width: 100vw;
+    max-width: 100%;
     height: 3.5rem;
     padding: ${({ theme }) => theme.padding.sm};
-    background-color: ${({ theme }) => theme.palette.main};
+    background-color: ${({ theme }) => theme.palette.primary.original};
 `;
