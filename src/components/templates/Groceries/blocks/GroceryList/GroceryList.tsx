de529170b990ba11476610691c43130b94884ad5
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 14:59:37 +0100

    Update structure: readability

--- src/components/templates/Groceries/blocks/GroceryList/GroceryList.tsx
+++ src/components/templates/Groceries/blocks/GroceryList/GroceryList.tsx
@@ -13,24 +13,37 @@ export default function GroceryList(props: Props) {
     const { isOpen, toggleIsOpen } = useDisclosure();
     const { groceries } = useGroceriesContext();
 
-    const groceryList = groceries.filter(item => isCheckedList ? item.isChecked : !item.isChecked);
-    return (
-        <List>
-            {
-                isCheckedList && (
-                    <Wrapper onClick={toggleIsOpen}>
-                        Marked Items
-                        <Icon name={isOpen ? 'chevronUp' : 'chevronDown'} />
-                    </Wrapper>
-                )
-            }
-            {
-                ((isCheckedList && isOpen) || (!isCheckedList)) && groceryList.map((grocery) => (
-                    <GroceryItem
-                        key={grocery.id}
-                        {...grocery} />
-                ))
-            }
-        </List>
+    const groceryList = groceries.filter(item =>
+        isCheckedList ? item.isChecked : !item.isChecked
     );
+
+    if (isCheckedList) {
+        return (
+            <List>
+                {
+                    groceryList.length > 0 && (
+                        <Wrapper onClick={toggleIsOpen}>
+                            Marked Items
+                            <Icon name={isOpen ? 'chevronUp' : 'chevronDown'} />
+                        </Wrapper>
+                    )
+                }
+            </List>
+        );
+    } else {
+        return (
+            <List>
+                {
+                    groceryList.length > 0 ? (
+                        groceryList.map((grocery) => (
+                            <GroceryItem
+                                key={grocery.id}
+                                {...grocery} />
+                        ))) : (
+                        <Wrapper>No item</Wrapper>
+                    )
+                }
+            </List>
+        );
+    }
 }
