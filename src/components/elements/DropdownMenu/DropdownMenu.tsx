aac78d56a60944c9ba60908035eb516e5885b405
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 14:47:58 +0100

    Add stylings: dropdown menu

--- src/components/elements/DropdownMenu/DropdownMenu.tsx
+++ src/components/elements/DropdownMenu/DropdownMenu.tsx
@@ -1,6 +1,43 @@
-type Props = {}
-export default function DropdownMenu({ }: Props) {
+import { useDisclosure } from '../../../hooks';
+import useOutsideDetector from '../../../hooks/useOutsideDetector';
+import { IconButton } from '../Button';
+import { Menu, MenuItem, Wrapper } from './styled';
+
+export interface MenuItem {
+    label: string;
+    onClick: () => void;
+    isDisabled?: boolean,
+}
+
+type Props = {
+    contents: MenuItem[];
+}
+
+export default function DropdownMenu(props: Props) {
+    const { contents } = props;
+    const { isOpen, toggleIsOpen, onClose } = useDisclosure();
+    const { containerRef } = useOutsideDetector(onClose);
+
     return (
-        <div>DropdownMenu</div>
+        <Wrapper>
+            <IconButton
+                name='threeDots'
+                variant='ghost'
+                onClick={toggleIsOpen} />
+            {isOpen && (
+                <Menu ref={containerRef as React.RefObject<HTMLDivElement>}>
+                    {
+                        contents.map(({ label, onClick, isDisabled }) => (
+                            <MenuItem
+                                key={label}
+                                disabled={isDisabled}
+                                onClick={onClick}>
+                                {label}
+                            </MenuItem>
+                        ))
+                    }
+                </Menu>
+            )}
+        </Wrapper>
     );
 }
