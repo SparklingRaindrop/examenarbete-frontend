d75dad6b281a23250ba2de2bc27ff0af08a8de2b
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 14:59:52 +0100

    Change top-margin

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const Wrapper = styled.main`
     min-height: 100vh;
     max-width: 100vw;
-    margin-top: 3.5rem; // Header height
+    margin-top: 2rem;
     padding: ${({ theme }) => theme.padding.md};
 
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 2rem;
 `;
