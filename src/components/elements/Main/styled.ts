a4bc56a5187946ec4b73c9974614b8359ce65da3
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 09:36:44 +0100

    Update stylings: overflow

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.main`
     min-height: 100vh;
+    max-width: 100vw;
     margin-top: 3.5rem; // Header height
     padding: ${({ theme }) => theme.padding.md};
+    
+    overflow-x: hidden;
+    overflow-y: auto;
 
     display: flex;
     flex-direction: column;
