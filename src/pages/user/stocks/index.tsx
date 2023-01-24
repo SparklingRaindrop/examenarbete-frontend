65d449a1f0ca637a8fb48685d239773a00d5b9cf
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-24 16:44:46 +0100

    Add fetch on items

--- src/pages/user/stocks/index.tsx
+++ src/pages/user/stocks/index.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
 import StockManager from '../../../components/templates/StockManager/StockManager';
+import { useRecipesContext } from '../../../hooks';
 import useStocksContext from '../../../hooks/useStocksContext';
 
 export default function StocksPage() {
     const { getStocks } = useStocksContext();
+    const { getItems } = useRecipesContext();
 
     useEffect(() => {
         getStocks();
+        getItems();
         // eslint-disable-next-line
     }, []);
     return (
