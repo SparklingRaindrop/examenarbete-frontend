c0f4ca1575a233a725ce376a005bc81794bc9af9
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-25 11:38:08 +0100

    Update styling: Text

--- src/components/elements/Text/styled.ts
+++ src/components/elements/Text/styled.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const Text = styled.p`
-    width: 100%;
+    max-width: 100%;
+    display: inline-block;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    word-break: normal;
+    line-break: strict;
+    hyphens: none;
+    -webkit-hyphens: none;
+    -moz-hyphens: none;
 `;
