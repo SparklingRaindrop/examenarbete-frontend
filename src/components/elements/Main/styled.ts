ee291a6304eaed04c4555b163391dbaa11429fd0
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-26 21:28:36 +0100

    Add margin-bottom

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -5,6 +5,7 @@ export const Wrapper = styled.main`
     min-height: calc(100vh - 3.5rem); /* 3.5rem = header height */
     margin: auto;
     padding: ${({ theme }) => theme.padding.md};
+    margin-bottom: 3rem;
 
     @media ${screenSize.md} {
         max-width: 62.5rem;
