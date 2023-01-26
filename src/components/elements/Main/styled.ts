421d7175f9affbe9f35391664a169a5af9ee32e0
Tsubasa <tsubasa.fujii@cmeducations.se>
2023-01-26 15:35:08 +0100

    height bug fix

--- src/components/elements/Main/styled.ts
+++ src/components/elements/Main/styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { screenSize } from '../../../util/mediaQuery';
 
 export const Wrapper = styled.main`
-    height: calc(100vh - 3.5rem); /* 3.5rem = header height */
+    min-height: calc(100vh - 3.5rem); /* 3.5rem = header height */
     margin: auto;
     padding: ${({ theme }) => theme.padding.md};
 
